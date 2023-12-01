380cdc487e66977311874d3a6809ae70bd629d09
mouna296 <saimouna999@gmail.com>
2023-11-30 19:02:17 -0800

    date selector

--- client/src/components/DateSelector.jsx
+++ client/src/components/DateSelector.jsx
@@ -1,184 +1,82 @@
-import { ArrowPathIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { AuthContext } from '../context/AuthContext';
 
 const DateSelector = ({ selectedDate, setSelectedDate }) => {
-	const { auth } = useContext(AuthContext)
-	const wrapperRef = useRef(null)
-	const [isEditing, SetIsEditing] = useState(false)
+    const { auth } = useContext(AuthContext);
+    const wrapperRef = useRef(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [dates, setDates] = useState([]);
 
-	const handlePrevDay = () => {
-		const prevDay = new Date(selectedDate)
-		prevDay.setDate(prevDay.getDate() - 1)
-		setSelectedDate(prevDay)
-		sessionStorage.setItem('selectedDate', prevDay)
-	}
+    const handlePrevDay = () => {
+        const prevDay = new Date(selectedDate);
+        prevDay.setDate(prevDay.getDate() - 1);
+        setSelectedDate(prevDay);
+    };
 
-	const handleNextDay = () => {
-		const nextDay = new Date(selectedDate)
-		nextDay.setDate(nextDay.getDate() + 1)
-		setSelectedDate(nextDay)
-		sessionStorage.setItem('selectedDate', nextDay)
-	}
+    const handleNextDay = () => {
+        const nextDay = new Date(selectedDate);
+        nextDay.setDate(nextDay.getDate() + 1);
+        setSelectedDate(nextDay);
+    };
 
-	const handleToday = () => {
-		const today = new Date()
-		setSelectedDate(today)
-		sessionStorage.setItem('selectedDate', today)
-	}
+    const formatDate = (date) => {
+        return date.toLocaleDateString('en-CA', { weekday: 'short', month: 'short', day: 'numeric' });
+    };
 
-	const formatDate = (date) => {
-		const weekday = date.toLocaleString('default', { weekday: 'long' })
-		const day = date.getDate()
-		const month = date.toLocaleString('default', { month: 'long' })
-		const year = date.getFullYear()
-		return `${weekday} ${day} ${month} ${year}`
-	}
+    const DateShort = ({ date }) => {
+        const isThisDate =
+            selectedDate.getDate() === date.getDate() &&
+            selectedDate.getMonth() === date.getMonth() &&
+            selectedDate.getFullYear() === date.getFullYear();
 
-	const DateShort = ({ date, selectedDate }) => {
-		const day = date.getDate()
-		const weekday = date.toLocaleString('default', { weekday: 'short' })
+        return (
+            <button
+                className={`flex items-center justify-center rounded p-1 font-semibold focus:outline-none ${
+					isThisDate ? 'bg-green-500 text-white' : 'text-black'
+				} bg-gray-500 hover:bg-gray-600 shadow-md transition duration-300 ease-in-out transform hover:scale-105`}
+                onClick={() => {
+                    setSelectedDate(date);
+                    setIsEditing(false);
+                }}
+            >
+                {formatDate(date)}
+            </button>
+        );
+    };
 
-		const isThisDate =
-			selectedDate.getDate() === date.getDate() &&
-			selectedDate.getMonth() === date.getMonth() &&
-			selectedDate.getFullYear() === date.getFullYear()
+    useEffect(() => {
+        const today = new Date();
+        const weekDates = [];
+        for (let i = -3; i < 4; i++) {
+            const newDate = new Date(today);
+            newDate.setDate(selectedDate.getDate() + i);
+            weekDates.push(newDate);
+        }
+        setDates(weekDates);
+    }, [selectedDate]);
 
-		const isToday = new Date(date).setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0)
+    return (
+        <div className="flex flex-col gap-2 items-center">
+            {/* Previous and Next Day Buttons */}
+            <div className="flex items-center justify-between gap-2 rounded-md bg-gray-200 p-2 font-semibold w-full">
+                <button onClick={handlePrevDay} className="hover:bg-gray-300 rounded-full p-1">
+                    <ChevronLeftIcon className="h-6 w-6 text-gray-700" />
+                </button>
+                <div className="text-lg w-24 text-center">{formatDate(selectedDate)}</div>
+                <button onClick={handleNextDay} className="hover:bg-gray-300 rounded-full p-1">
+                    <ChevronRightIcon className="h-6 w-6 text-gray-700" />
+                </button>
+            </div>
 
-		return (
-			<button
-				title={formatDate(date)}
-				className={`flex min-w-[48px] flex-col items-center justify-center rounded p-1 font-semibold ${
-					isThisDate
-						? 'bg-gradient-to-br from-red-800 to-red-700 text-white'
-						: isToday
-						? 'bg-gradient-to-br from-slate-100 to-white ring-2 ring-inset ring-indigo-800 hover:from-white hover:to-white'
-						: isPast(date)
-						? 'bg-gradient-to-br from-gray-600 to-gray-500 text-white hover:from-gray-500 hover:to-gray-400'
-						: 'bg-to-white hover:from-white hover:to-white'
-				}`}
-				onClick={() => {
-					setSelectedDate(date)
-					sessionStorage.setItem('selectedDate', date)
-				}}
-			>
-				<p className="text-sm">{weekday}</p>
-				<p className="text-xl">{day}</p>
-			</button>
-		)
-	}
+            {/* Date Selection */}
+            <div className="flex gap-2 overflow-x-auto items-center justify-center w-full">
+                {dates.map((date, index) => (
+                    <DateShort key={index} date={date} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
-	const isPast = (date) => {
-		return new Date(date).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
-	}
-
-	const handleChange = (event) => {
-		setSelectedDate(new Date(event.target.value))
-	}
-
-	function generateDateRange(startDate, endDate) {
-		const dates = []
-		const currentDate = new Date(startDate)
-
-		while (currentDate <= endDate) {
-			dates.push(new Date(currentDate.getTime()))
-			currentDate.setDate(currentDate.getDate() + 1)
-		}
-
-		return dates
-	}
-
-	function getPastAndNextDateRange() {
-		const today = new Date()
-		const pastDays = new Date(today)
-		if (auth.role === 'admin') {
-			pastDays.setDate(today.getDate() - 7)
-		}
-
-		const nextDays = new Date(today)
-		nextDays.setDate(today.getDate() + 14)
-
-		return generateDateRange(pastDays, nextDays)
-	}
-
-	useEffect(() => {
-		document.addEventListener('click', handleClickOutside, false)
-		return () => {
-			document.removeEventListener('click', handleClickOutside, false)
-		}
-	}, [])
-
-	const handleClickOutside = (event) => {
-		if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-			SetIsEditing(false)
-		}
-	}
-
-	return (
-		<div className="flex flex-col gap-2">
-			<div className="relative flex items-stretch justify-between gap-2 rounded-md bg-gradient-to-br from-slate-800 to-slate-700 p-2 font-semibold text-white">
-				{auth.role === 'admin' || !isPast(new Date().setDate(selectedDate.getDate() - 1)) ? (
-					<button
-						title="Go to yesterday"
-						className={'rounded hover:bg-gradient-to-br hover:from-lime-600 hover:to-lime-600'}
-						onClick={handlePrevDay}
-					>
-						<ChevronLeftIcon className="h-10 w-10 text-white" />
-					</button>
-				) : (
-					<div className="h-10 w-10"></div>
-				)}
-
-				{isEditing ? (
-					<div className="w-full" ref={wrapperRef}>
-						<input
-							title="Select date"
-							type="Date"
-							min={auth.role !== 'admin' && new Date().toLocaleDateString('en-CA')}
-							required
-							autoFocus
-							className={`w-full rounded border border-white bg-gradient-to-br from-red-800 to-red-700 px-1 text-center text-2xl font-semibold drop-shadow-sm sm:text-3xl`}
-							value={selectedDate.toLocaleDateString('en-CA')}
-							onChange={handleChange}
-							style={{ colorScheme: 'dark' }}
-						/>
-					</div>
-				) : (
-					<div
-						className="flex w-full items-center justify-center rounded text-center text-xl hover:bg-lime-600  sm:text-2xl"
-						onClick={() => {
-							SetIsEditing(true)
-						}}
-					>
-						{formatDate(selectedDate)}
-					</div>
-				)}
-
-				<div className="flex items-center justify-between gap-2">
-					<button
-						title="Go to tomorrow"
-						className="rounded hover:bg-gradient-to-br hover:from-lime-600 hover:to-lime-600"
-						onClick={handleNextDay}
-					>
-						<ChevronRightIcon className="h-10 w-10 text-white" />
-					</button>
-					<button
-						title="Go to today"
-						className="rounded px-1 hover:bg-gradient-to-br hover:from-lime-600 hover:to-lime-600"
-						onClick={handleToday}
-					>
-						<ArrowPathIcon className="h-10 w-10 text-white" />
-					</button>
-				</div>
-			</div>
-			<div className="flex gap-2 overflow-auto">
-				{getPastAndNextDateRange().map((date, index) => (
-					<DateShort key={index} date={date} selectedDate={selectedDate} />
-				))}
-			</div>
-		</div>
-	)
-}
-
-export default DateSelector
+export default DateSelector;
